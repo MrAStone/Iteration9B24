1d63f77e2997a9a8216b1a2d4c17e9399602d15d
Al Stone <astone@heckgrammar.co.uk>
2024-10-07 11:11:35 +0100

    Add project files.

---
+++ Iteration9B24/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Iteration9B24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                int aNumber = 12;
+                Console.WriteLine(i);
+            }
+            for(int i = 1;i<=10; i += 4)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
